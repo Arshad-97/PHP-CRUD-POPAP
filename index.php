949b6f9638db1f35a2fbded4bdc9899fcd6f33ae
Arshad-97 <agmarshad97@gmail.com>
2020-10-26 17:00:24 +0530

    Successfully Created popup form

--- index.php
+++ index.php
@@ -11,10 +11,61 @@
 
 <body>
 
+
+
+
+    <!-- Modal -->
+    <div class="modal fade" id="studentaddmodel" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Add student data</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <form action="" method="POST">
+                    <div class="modal-body">
+
+                        <div class="form-group">
+                            <label>First Name</label>
+                            <input type="text" class="form-control" name="fname" placeholder="Enter First Name">
+                        </div>
+                        <div class="form-group">
+                            <label>Last Name</label>
+                            <input type="text" class="form-control" name="lname" placeholder="Enter Last Name">
+                        </div>
+                        <div class="form-group">
+                            <label>Course</label>
+                            <input type="text" class="form-control" name="fname" placeholder="Enter Course Name">
+                        </div>
+                        <div class="form-group">
+                            <label>Phone Number</label>
+                            <input type="number" class="form-control" name="fname" placeholder="Enter Phone Number">
+                        </div>
+
+
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="button" name="insertdata" class="btn btn-primary">Add Data</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
     <div class="container">
         <div class="jumbotron">
             <div class="card">
                 <h2>PHP CRUD Bootstrap Model</h2>
+            </div>
+            <div class="card">
+                <div class="card-body">
+                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#studentaddmodel">
+                        Add Data
+                    </button>
+                </div>
             </div>
         </div>
 
